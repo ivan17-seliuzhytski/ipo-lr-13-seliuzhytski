c300b4bcb90df82787787aff979a4adccecb7f80
Ivan <ivan.seliuzhytski17@gmail.com>
2024-12-22 20:11:36 +0300

    Update image_handler.py

--- paket/image_handler.py
+++ paket/image_handler.py
@@ -1,14 +1,12 @@
 from PIL import Image
 import datetime
-
+import os
 class ImageHandler:
-    def __init__(self, image_path):
-        self.image_path = image_path
+    def __init__(self):
         self.image = None
 
-    def load_image(self):
-        self.image = Image.open(self.image_path)
-        return self.image
+    def load_image(self, image_path):
+        self.image = Image.open(image_path)
 
     def save_image(self, output_path):
         self.image.save(output_path)
@@ -18,8 +16,12 @@ class ImageHandler:
         new_size = (int(width * scale_factor), int(height * scale_factor))
         self.image = self.image.resize(new_size)
         return self.image
+    
+    def get_image(self):
+        return self.image
 
     def save_with_date(self):
         current_date = datetime.datetime.now().strftime("%Y-%m-%d")
-        output_path = self.image_path.split('.')[0] + f"_{current_date}." + self.image_path.split('.')[1]
+        filename, ext = os.path.splitext(output_path)
+        output_path = self.image_path.split('.')[0] + f"{filename}_{current_date}{ext}"
         self.save_image(output_path)
