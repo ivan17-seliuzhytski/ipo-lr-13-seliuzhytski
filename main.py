ea6c3dee82a5345e9a4157d2d0ee22caa4b1ab6b
Ivan <ivan.seliuzhytski17@gmail.com>
2024-12-22 18:26:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import tkinter as tk
-from tkinter import filedialog, messagebox
-from PIL import ImageTk, Image, ImageFilter
 from paket.image_handler import ImageHandler
 from paket.image_processor import ImageProcessor
-import os
 
 if __name__ == "__main__":
     handler = ImageHandler("paket/image_1.jpg")
