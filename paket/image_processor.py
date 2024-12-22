5c76b22eb00d1cf8326d0fd9421b08724aa8c836
Ivan <ivan.seliuzhytski17@gmail.com>
2024-12-22 20:12:06 +0300

    Update image_processor.py

--- paket/image_processor.py
+++ paket/image_processor.py
@@ -6,7 +6,26 @@ class ImageProcessor:
 
     def apply_emboss_filter(self):
         self.image = self.image.filter(ImageFilter.EMBOSS)
-        return self.image
+        return self.imagefrom PIL import ImageFilter, ImageDraw, ImageFont
+
+class ImageProcessor:
+    def __init__(self, image):
+        self.image = image
+
+    def apply_emboss(self):
+        if self.image:
+            self.image = self.image.filter(ImageFilter.EMBOSS)
+
+    def add_text(self, text="Вариант 5", font_path='arial.ttf', font_size=20):
+        if self.image:
+            draw = ImageDraw.Draw(self.image)
+            font = ImageFont.truetype(font_path, font_size)
+            # Исправляем на использование textbbox
+            text_bbox = draw.textbbox((0, 0), text, font=font)
+            text_width = text_bbox[2] - text_bbox[0]
+            text_height = text_bbox[3] - text_bbox[1]
+            position = (self.image.width - text_width - 10, self.image.height - text_height - 10)
+            draw.text(position, text, font=font, fill='white')
 
     def add_watermark(self, text="Вариант 5"):
         width, height = self.image.size
